fb3beeafcf2ff2edbd6666459e0bf6f89727aa47
UltraFutile <ultrafutile@protonmail.com>
2022-05-29 11:50:43 -0500

    Color the board sqaures

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,14 +3,16 @@ import './Board.css'
 import { Square } from "./Square";
 
 export class Board extends React.Component {
-    renderSquare(file: number, rank: number) {
-        return <Square file={file} rank={rank} /> //'\u265C' for black knight
+    renderSquare(file: number, rank: number, color: string) {
+        return <Square file={file} rank={rank} color={color} /> //'\u265C' for black knight
     }
 
     renderRow(rank: number) {
+        let colorIn: boolean = rank % 2 === 0;
         const row = [];
         for (let i = 0; i < 8; i++) {
-            row.push(this.renderSquare(i, rank));
+            row.push(this.renderSquare(i, rank, colorIn ? '#519e47' : '#FFFDD0'));
+            colorIn = !colorIn;
         }
         return (
             <div className="board-row">
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import './Square.css'
 
-export class Square extends React.Component<{ file: number, rank: number }> {
+export class Square extends React.Component<{ file: number, rank: number, color: string }> {
     FILE_CHAR_STRING = "ABCDEFGH";
 
     render() {
+        const buttonStyle = {
+            backgroundColor: this.props.color
+        };
         return (
-            <button className="square">
+            <button className="square" style={buttonStyle}>
                 {/* {'\u265C'} */}
-                {`${this.FILE_CHAR_STRING[this.props.file]}${this.props.rank + 1}`}
+                {/* {`${this.FILE_CHAR_STRING[this.props.file]}${this.props.rank + 1}`} */}
+                {`${this.props.file}${this.props.rank}`}
             </button>
         );
     }
