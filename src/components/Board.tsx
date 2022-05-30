279f247695f0deb2f4cc6508af34640a58737643
UltraFutile <ultrafutile@protonmail.com>
2022-05-29 20:26:47 -0500

    Create board state, state lifted to board FC

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,33 +1,95 @@
+import React from 'react';
 import './Board.css'
 import { Square } from "./Square";
 
+/**
+ * Should maintain state of:
+ *  - Board
+ *  - Able to lookup any random square in constant time
+ * 
+ * Game manager should be able to:
+ *  - Use board to find all pieces
+ * 
+ */
 export const Board = () => {
+    const [boardState, setBoardState] = React.useState<BoardState>(new BoardState(8, 8));
+
+    const onSquareClick = (file: number, rank: number) => {
+        console.log(`FILE: ${file}, RANK: ${rank}`)
+    };
+
     const renderSquare = (file: number, rank: number, color: string) => {
-        return <Square file={file} rank={rank} color={color} /> //'\u265C' for black knight
+        return <Square 
+            key={`${file}${rank}`} 
+            file={file} 
+            rank={rank} 
+            color={color} 
+            onClick={onSquareClick}
+            
+        />; //'\u265C' for black knight
     }
 
-    const renderRow = (rank: number) => {
-        let colorIn: boolean = rank % 2 === 0;
-        const row = [];
-        for (let i = 0; i < 8; i++) {
-            row.push(renderSquare(i, rank, colorIn ? '#BA8C63' : '#FFFDD0'));
-            colorIn = !colorIn;
+    const renderSquares = () => {
+        const rows = [];
+        for (let i = 0; i < boardState.fileNum; i++) {
+            const row = [];
+            for (let j = 0; j < boardState.rankNum; j++) {
+                let squareState: SquareState = boardState.getSquare(i, j);
+                row.push(renderSquare(squareState.file, squareState.rank, squareState.color));
+            }
+            rows.unshift(
+                <div key={i} className="board-row">
+                    {row}
+                </div>
+            )
         }
-        return (
-            <div className="board-row">
-                {row}
-            </div>
-        );
-    }
-
-    const rows = [];
-    for (let i = 0; i < 8; i++) {
-        rows.unshift(renderRow(i));
+        return rows;
     }
 
     return (
-        <div>
-            {rows}
+        <div className="board-shell">
+            {renderSquares()}
         </div>
     );
+}
+
+interface SquareState {
+    color: string;
+    file: number;
+    rank: number;
+}
+
+class BoardState {
+    private squareArray: SquareState[][];
+    public fileNum: number;
+    public rankNum: number;
+
+    constructor(fileNum: number = 8, rankNum: number = 8) {
+        this.squareArray = this.initializeSquares(fileNum, rankNum);
+        this.fileNum = fileNum;
+        this.rankNum = rankNum;
+    }
+
+    initializeSquares(fileNum: number, rankNum: number) {
+        const rows = [];
+        for (let i = 0; i < fileNum; i++) {
+            let row: SquareState[] = [];
+            let colorIn: boolean = i % 2 === 0;
+            for (let j = 0; j < rankNum; j++) {
+                row.push({
+                    color:  colorIn ? '#BA8C63' : '#FFFAF0',
+                    file: i,
+                    rank: j
+                });
+                colorIn = !colorIn;
+            }
+            rows.push(row);
+        }
+    
+        return rows;
+    }
+
+    getSquare(file: number, rank: number) {
+        return this.squareArray[file][rank];
+    }
 }
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,12 +1,12 @@
 import './Square.css'
 
-export const Square = (props: { file: number, rank: number, color: string }) => {
+export const Square = (props: { file: number, rank: number, color: string, onClick: (file: number, rank: number) => void}) => {
     const FILE_CHAR_STRING = "ABCDEFGH";
     const buttonStyle = {
         backgroundColor: props.color
     };
     return (
-        <button className="square" style={buttonStyle}>
+        <button className="square" style={buttonStyle} onClick={() => props.onClick(props.file, props.rank)}>
             {/* {'\u265C'} */}
             {/* {`${this.FILE_CHAR_STRING[this.props.file]}${this.props.rank + 1}`} */}
             {`${props.file}${props.rank}`}
