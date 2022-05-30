1bd0b3ab3a2205392c6aadabb35530bb08441f0c
UltraFutile <ultrafutile@protonmail.com>
2022-05-29 23:50:03 -0500

    Add free version of fontawesome for their nice chess icons

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,3 +1,7 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChessKing, faChessKnight } from '@fortawesome/free-solid-svg-icons'
+// import { faChessKing as regularKing } from '@fortawesome/free-regular-svg-icons'
+
 import './Square.css'
 
 export const Square = (props: { file: number, rank: number, color: string, onClick: (file: number, rank: number) => void}) => {
@@ -9,7 +13,11 @@ export const Square = (props: { file: number, rank: number, color: string, onCli
         <button className="square" style={buttonStyle} onClick={() => props.onClick(props.file, props.rank)}>
             {/* {'\u265C'} */}
             {/* {`${this.FILE_CHAR_STRING[this.props.file]}${this.props.rank + 1}`} */}
-            {`${props.file}${props.rank}`}
+            <FontAwesomeIcon icon={faChessKnight} style={{ 
+                color: "white",
+                filter: "drop-shadow(2px 2px 5px rgb(0 0 0 / 0.4))"
+            }}/>
+            {/* {`${props.file}${props.rank}`} */}
         </button>
     );
 }
