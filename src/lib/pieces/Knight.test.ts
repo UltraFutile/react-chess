cd786f1175c9c2b2292669a85a40e579a3a40e97
UltraFutile <ultrafutile@protonmail.com>
2022-06-05 18:30:20 -0500

    Add move validation for knight piece

---
+++ src/lib/pieces/Knight.test.ts
@@ -0,0 +1,130 @@
+import { addPiece } from "../../components/board/ManagePiece";
+import { Coordinates } from "../../types/AlgebraicNotation";
+import { BoardStateFactory, BoardState } from "../model/BoardState";
+import { Team } from "../Team";
+import { isLegalKnightMove } from "./Knight";
+
+describe.each([
+    { thisTeam: Team.White, otherTeam: Team.Black},
+    { thisTeam: Team.Black, otherTeam: Team.White}
+])("knight tests", ({ thisTeam, otherTeam }) => {
+    const boardFactory: BoardStateFactory = new BoardStateFactory();
+    let state: BoardState;
+
+    beforeEach(() => {
+        state = boardFactory.createBoardState();
+    });
+
+    describe("legal basic moves", () => {
+        const origin: Coordinates = ['e', 4];
+        const knightLegalDestinations: Coordinates[] = [
+            ['f', 6], ['g', 5], ['g', 3], ['f', 2], 
+            ['d', 2], ['c', 3], ['c', 5], ['d', 6]
+        ];
+
+        beforeEach(() => {
+            addPiece(state, thisTeam, 'knight', origin);
+        });
+        
+        it.each([
+            knightLegalDestinations.map((value) => { return {coordinates: value}; })
+        ])("can move two squares and one diagonal to (%j)", ({ coordinates }) => {
+            expect(isLegalKnightMove(state, origin, coordinates)).toBe(true);
+        });
+    });
+
+    describe("illegal captures", () => {
+        const origin: Coordinates = ['e', 4];
+        const knightLegalDestinations: Coordinates[] = [
+            ['f', 6], ['g', 5], ['g', 3], ['f', 2], 
+            ['d', 2], ['c', 3], ['c', 5], ['d', 6]
+        ];
+
+        beforeEach(() => {
+            addPiece(state, thisTeam, 'knight', origin);
+            knightLegalDestinations.forEach((value: Coordinates) => {
+                addPiece(state, thisTeam, 'pawn',  value);
+            });
+        });
+        
+        it.each([
+            knightLegalDestinations.map((value) => { return {coordinates: value}; })
+        ])("can not capture piece of same team at (%j)", ({ coordinates }) => {
+            expect(isLegalKnightMove(state, origin, coordinates)).toBe(false);
+        });
+    });
+
+    describe("legal captures", () => {
+        const origin: Coordinates = ['e', 4];
+        const knightLegalDestinations: Coordinates[] = [
+            ['f', 6], ['g', 5], ['g', 3], ['f', 2], 
+            ['d', 2], ['c', 3], ['c', 5], ['d', 6]
+        ];
+
+        beforeEach(() => {
+            addPiece(state, thisTeam, 'knight', origin);
+            knightLegalDestinations.forEach((value: Coordinates) => {
+                addPiece(state, otherTeam, 'pawn',  value);
+            });
+        });
+        
+        it.each([
+            knightLegalDestinations.map((value) => { return {coordinates: value}; })
+        ])("can capture enemey pieces at (%j)", ({ coordinates }) => {
+            expect(isLegalKnightMove(state, origin, coordinates)).toBe(true);
+        });
+    });
+
+    describe("illegal short moves", () => {
+        const origin: Coordinates = ['e', 4];
+        const immediateSurroundingSquares: Coordinates[] = [
+            ['e', 5], ['f', 5], ['f', 4], ['f', 3], 
+            ['e', 3], ['d', 3], ['d', 4], ['d', 5 ]
+        ];
+
+        beforeEach(() => {
+            addPiece(state, thisTeam, 'knight', origin);
+        });
+        
+        it.each([
+            immediateSurroundingSquares.map((value) => { return {coordinates: value}; })
+        ])("can't move to immediate square at (%j)", ({ coordinates }) => {
+            expect(isLegalKnightMove(state, origin, coordinates)).toBe(false);
+        });
+    });
+
+    describe("illegal straight 2 square moves", () => {
+        const origin: Coordinates = ['e', 4];
+        const immediateSurroundingSquares: Coordinates[] = [
+            ['e', 6], ['g', 4], ['e', 2], ['c', 4],
+        ];
+
+        beforeEach(() => {
+            addPiece(state, thisTeam, 'knight', origin);
+        });
+        
+        it.each([
+            immediateSurroundingSquares.map((value) => { return {coordinates: value}; })
+        ])("can't move only two squares to (%j)", ({ coordinates }) => {
+            expect(isLegalKnightMove(state, origin, coordinates)).toBe(false);
+        });
+    });
+
+    describe("illegal destinations immediately out of range", () => {
+        const origin: Coordinates = ['e', 4];
+        const immediateSurroundingSquares: Coordinates[] = [
+            ['e', 7], ['g', 6], ['h', 4], ['g', 2], 
+            ['e', 1], ['c', 2], ['b', 4], ['c', 6],
+        ];
+
+        beforeEach(() => {
+            addPiece(state, thisTeam, 'knight', origin);
+        });
+        
+        it.each([
+            immediateSurroundingSquares.map((value) => { return {coordinates: value}; })
+        ])("can't move out of range to (%j)", ({ coordinates }) => {
+            expect(isLegalKnightMove(state, origin, coordinates)).toBe(false);
+        });
+    });
+})
---
+++ src/lib/pieces/Knight.ts
@@ -0,0 +1,26 @@
+import { Coordinates } from "../../types/AlgebraicNotation";
+import { BoardState, getMovement, getSquare } from "../model/BoardState";
+import { SquareState } from "../model/SquareState";
+import { Team } from "../Team";
+import { destinationHasSameTeam } from "./PieceHelpers";
+
+export function isLegalKnightMove(boardState: BoardState, orig: Coordinates, dest: Coordinates) {
+    const origin: SquareState = getSquare(boardState, orig);
+    const destination: SquareState = getSquare(boardState, dest);
+    
+    if (origin.piece == null) {
+        throw new Error(`Square at (${origin.file}, ${origin.rank}) does not have a piece!`)
+    }
+
+    const originTeam: Team = origin.piece.team;
+    
+    if (destinationHasSameTeam(originTeam, destination))
+        return false;
+
+    const [fileMovement, rankMovement] = getMovement(orig, dest);
+    const fileMagnitude: number = Math.abs(fileMovement);
+    const rankMagnitude: number = Math.abs(rankMovement);
+
+    return ((fileMagnitude === 2 && rankMagnitude === 1)
+            || (fileMagnitude === 1 && rankMagnitude === 2));
+}
