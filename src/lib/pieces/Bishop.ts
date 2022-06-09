8dc900959f8f5bffc70948de54efa0be7d12a41a
UltraFutile <ultrafutile@protonmail.com>
2022-06-08 22:58:12 -0500

    remove next square team check from bishop code

--- src/lib/pieces/Bishop.ts
+++ src/lib/pieces/Bishop.ts
@@ -1,23 +1,9 @@
 import { Coordinates } from "../../types/AlgebraicNotation";
 import { diagonalMovementGenerator } from "../BoardIterators";
-import { BoardState, getMovement, getSquare } from "../model/BoardState";
-import { SquareState } from "../model/SquareState";
-import { Team } from "../Team";
-import { destinationHasSameTeam } from "./PieceHelpers";
+import { BoardState, getMovement } from "../model/BoardState";
 
 export function isLegalBishopMove(boardState: BoardState, orig: Coordinates, dest: Coordinates) {
-    const origin: SquareState = getSquare(boardState, orig);
-    const destination: SquareState = getSquare(boardState, dest);
 
-    if (origin.piece == null) {
-        throw new Error(`Square at (${origin.file}, ${origin.rank}) does not have a piece!`)
-    }
-    
-    const originTeam: Team = origin.piece.team;
-    
-    if (destinationHasSameTeam(originTeam, destination))
-        return false;
-    
     const [fileMovement, rankMovement] = getMovement(orig, dest);
     const fileMagnitude: number = Math.abs(fileMovement);
     const rankMagnitude: number = Math.abs(rankMovement);
