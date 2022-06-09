420f5bc53a6de04e762a1567a8353a40dbf19b0a
UltraFutile <ultrafutile@protonmail.com>
2022-06-08 23:19:35 -0500

    simplify isLegalRookMove

--- src/lib/pieces/Rook.ts
+++ src/lib/pieces/Rook.ts
@@ -3,15 +3,7 @@ import { BoardState, getMovement } from "../model/BoardState";
 import { isClearStraightPath } from "./PieceHelpers";
 
 export function isLegalRookMove(boardState: BoardState, orig: Coordinates, dest: Coordinates): boolean {
-    const [fileMovement, rankMovement] = getMovement(orig, dest);
-    const fileMagnitude: number = Math.abs(fileMovement);
-    const rankMagnitude: number = Math.abs(rankMovement);
-
-    const isMovingOnFile = fileMagnitude > 0 && rankMagnitude === 0;
-    const isMovingOnRank = fileMagnitude === 0 && rankMagnitude > 0;
-
-    if (!isMovingOnFile && !isMovingOnRank)
-        return false;
-
-    return isClearStraightPath(boardState, orig, dest);
+    const [fileMove, rankMove] = getMovement(orig, dest);
+    const isMovingStraight = fileMove * rankMove === 0 && fileMove + rankMove !== 0;
+    return isMovingStraight && isClearStraightPath(boardState, orig, dest);
 }
