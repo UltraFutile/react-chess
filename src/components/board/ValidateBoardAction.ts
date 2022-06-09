7a08f2b6d0c76b2fb2a1291a48f2afc899a228e2
UltraFutile <ultrafutile@protonmail.com>
2022-06-08 21:56:31 -0500

    use terms prevSquare and nextSquare

--- src/components/board/ValidateBoardAction.ts
+++ src/components/board/ValidateBoardAction.ts
@@ -11,20 +11,21 @@ import { Team } from '../../lib/Team';
 
 export const onSquareClickFactory = (state: BoardState, setState: React.Dispatch<React.SetStateAction<BoardState>>) => 
     (fileIndex: number, rankIndex: number) => () => {
-    const clickedSquare: SquareState = state.squareGrid[fileIndex][rankIndex];
-    const sameSquareSelected = clickedSquare.selected;
-    const noPieceSelected = state.currentlySelectedSquare == null && clickedSquare.piece == null;
+    const prevSquare = state.currentlySelectedSquare;
+    const nextSquare: SquareState = state.squareGrid[fileIndex][rankIndex];
+    const sameSquareSelected = nextSquare.selected;
+    const noPieceSelected = prevSquare == null && nextSquare.piece == null;
 
     if (noPieceSelected)
         return;
-    
+
     if (sameSquareSelected) {
         setState(unselectSquare(state, fileIndex, rankIndex));
     }
-    else if (isNewPieceSelected(state.currentlySelectedSquare, clickedSquare, state.whichTeamsTurn)) {
+    else if (isNewPieceSelected(prevSquare, nextSquare, state.whichTeamsTurn)) {
         setState(selectNewSquare(state, fileIndex, rankIndex));
     }
-    else if (validateMove(state, fileIndex, rankIndex)) {
+    else if (validateMove(prevSquare, nextSquare, state)) {
         setState(movePiece(state, fileIndex, rankIndex));
     }
 };
@@ -35,31 +36,28 @@ const isNewPieceSelected = (prevSquare: SquareState | undefined, nextSquare: Squ
     return noPieceSelectedYet && currentTeamPieceSelected;
 }
 
-const validateMove = (state: BoardState, file: number, rank: number): boolean => {
-    let prevSelectedSquare = state.currentlySelectedSquare;
-    if (prevSelectedSquare == null || prevSelectedSquare.piece == null) {
+const validateMove = (prevSquare: SquareState | undefined, nextSquare: SquareState, state: BoardState): boolean => {
+    if (prevSquare == null || prevSquare.piece == null) {
         throw new Error("Attempted to move piece without selecting one");
     }
 
-    const nextSquare: SquareState = state.squareGrid[file][rank];
-
     // validate move attempt
     let isValidMove: boolean = true;
-    switch(prevSelectedSquare.piece.piece) {
+    switch(prevSquare.piece.piece) {
         case 'pawn':
-            isValidMove = isLegalPawnMove(state, getCoordinates(prevSelectedSquare), getCoordinates(nextSquare));
+            isValidMove = isLegalPawnMove(state, getCoordinates(prevSquare), getCoordinates(nextSquare));
             break;
         case 'knight':
-            isValidMove = isLegalKnightMove(state, getCoordinates(prevSelectedSquare), getCoordinates(nextSquare));
+            isValidMove = isLegalKnightMove(state, getCoordinates(prevSquare), getCoordinates(nextSquare));
             break;
         case 'rook':
-            isValidMove = isLegalRookMove(state, getCoordinates(prevSelectedSquare), getCoordinates(nextSquare));
+            isValidMove = isLegalRookMove(state, getCoordinates(prevSquare), getCoordinates(nextSquare));
             break;
         case 'bishop':
-            isValidMove = isLegalBishopMove(state, getCoordinates(prevSelectedSquare), getCoordinates(nextSquare));
+            isValidMove = isLegalBishopMove(state, getCoordinates(prevSquare), getCoordinates(nextSquare));
             break;
         case 'king':
-            isValidMove = isLegalKingMove(state, getCoordinates(prevSelectedSquare), getCoordinates(nextSquare));
+            isValidMove = isLegalKingMove(state, getCoordinates(prevSquare), getCoordinates(nextSquare));
             break;
     }
 
